8bd0e762ccf1429dfd59de1cb58593312ff83f0a
Kenny Morris <kmorris1217@gmail.com>
2025-07-15 23:17:06 -0400

    Start building out auth route

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,29 +1,43 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
+import bcrypt
 
-auth_blueprint = Blueprint('/auth', __name__)
+auth_blueprint = Blueprint('auth', __name__, url_prefix='/auth')
 
-@auth_blueprint.route('/auth/signup', methods=['GET', 'POST'])
+@auth_blueprint.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        username = request.values.get('username')
-        email = request.values.get('email')
-        password = request.values.get('password')
-        password2 = request.values.get('password2')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
     return render_template('./auth/signup.html')
 
-@auth_blueprint.route('/auth/signin', methods=['GET', 'POST'])
+@auth_blueprint.route('/signin', methods=['GET', 'POST'])
 def signin():
     if request.method == 'POST':
-        username = request.values.get('username')
-        password = request.values.get('password')
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Get users password hash
+        stored_password = ""
+
+        stored_password_bytes = stored_password.encode('utf-8')
+        password_bytes = password.encode('utf-8')
+
+        if bcrypt.checkpw(password_bytes, stored_password_bytes):
+            print('User {username} has authenticated')
+            return redirect(url_for(''))
+        else:
+            print('User {username} has not authenticated')
+            return render_template('./auth/signin.html')
     return render_template('./auth/signin.html')
 
-@auth_blueprint.route('/auth/logout', methods=['GET', 'POST'])
+@auth_blueprint.route('/logout', methods=['GET', 'POST'])
 def logout():
     return render_template('./auth/logout.html')
 
-@auth_blueprint.route('/auth/forgotpassword', methods=['GET', 'POST'])
+@auth_blueprint.route('/forgotpassword', methods=['GET', 'POST'])
 def forgotPassword():
     if request.method == 'POST':
-        email = request.values.get('email')
+        email = request.form.get('email')
     return render_template('./auth/forgotpassword.html')
