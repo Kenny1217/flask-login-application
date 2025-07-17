25f09ab7ecefeb816941f888b4c8fea6194b1f23
Kenny Morris <kmorris1217@gmail.com>
2025-07-16 22:51:01 -0400

    Start database integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from .routes import auth, base
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def flask_app():
     app = Flask(__name__)
+
+    app.config.from_object('config.datbaseConfig')
+
+    db.init_app(app)
+
     app.register_blueprint(auth.auth_blueprint)
     app.register_blueprint(base.base_blueprint)
+
     return app
---
+++ config.py
@@ -0,0 +1,4 @@
+
+class datbaseConfig:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_app_database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
