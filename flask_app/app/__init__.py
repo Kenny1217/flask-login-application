a0739b1768f5e92c886c98fb1419ff29662cb158
Kenny Morris <kmorris1217@gmail.com>
2025-07-24 17:37:54 -0400

    Setup config file

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from .config import FlaskConfig
 from .routes import base
 
 def flask_app():
     app = Flask(__name__)
+
+    app.config.from_object(FlaskConfig)
 
     app.register_blueprint(base.base_blueprint)
 
