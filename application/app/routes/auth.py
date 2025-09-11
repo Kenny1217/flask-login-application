762f2b491eeae78eddf043fe8a7f81601051e513
Kenny Morris <kmorris1217@gmail.com>
2025-09-10 22:49:39 -0400

    Add auth placeholders

--- application/app/routes/auth.py
+++ application/app/routes/auth.py
@@ -9,3 +9,11 @@ def signup():
 @auth_blueprint.route('/signin', methods = ['GET', 'POST'])
 def signin():
     return render_template('./auth/signin.html')
+
+@auth_blueprint.route('/signout', methods = ['GET', 'POST'])
+def signout():
+    return render_template('./auth/signout.html')
+
+@auth_blueprint.route('/forgotpassword', methods = ['GET', 'POST'])
+def forgotpassword():
+    return render_template('./auth/forgotpassword.html')
