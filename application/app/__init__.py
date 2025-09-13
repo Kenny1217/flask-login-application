2b04a385e02e8c9adc6b6e614449179238cfc265
Kenny Morris <kmorris1217@gmail.com>
2025-09-12 20:32:34 -0400

    Set up flask config file

--- application/app/__init__.py
+++ application/app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from .routes import auth, base
+from app.config import FlaskConfig
 
 def flask_app():
     
     app = Flask(__name__)
+
+    app.config.from_object(FlaskConfig)
 
     app.register_blueprint(auth.auth_blueprint)
     app.register_blueprint(base.base_blueprint)
